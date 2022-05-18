d00206bc4698a8fe421da4476fa110fa04f4348f
lucascr <lcrodrigues1995@gmail.com>
2022-05-18 13:51:37 -0300

    Add html mocks, and run tests

--- src/modules/test.js
+++ src/modules/test.js
@@ -1,17 +1,54 @@
 const { addTask, deleteTask } = require('./add-remove.js');
 const { setStorage, getStorage } = require('./storage.js');
-
-
+const TASK = require('./tarea.js');
 
 describe('Add and Delete tasks', () => {
+  const htmlNoTask = `
+  <div id="to-do-list">
+      <div id="todo-title">
+        <p>Today´s To Do</p>
+        <span id="reload">&#x21bb;</span>
+      </div>
+      <div id="add">
+        <input id="addinput" type="text" placeholder="Add to your list..." value="Some input"/>
+        <span id="submittask">&#x21ea;</span>
+      </div>
+      <div id="list"></div>
+      <button id="clear" type="reset">Clear all completed</button>
+    </div>`;
+
+    const htmlWithTask = `
+    <div id="to-do-list">
+      <div id="todo-title">
+        <p>Today´s To Do</p>
+        <span id="reload">&#x21bb;</span>
+      </div>
+      <div id="add">
+        <input id="addinput" type="text" placeholder="Add to your list..." />
+        <span id="submittask">&#x21ea;</span>
+      </div>
+      <div id="list">
+        <div id="container">
+          <input type="checkbox" value="false" id="1">
+          <input id="1" placeholder="Some input">
+          <span style="display: block;">⋮</span>
+          <span id="1" class="trash" style="display: none;">🗑️</span>
+        </div>
+      </div>
+      <button id="clear" type="reset">Clear all completed</button>
+    </div>`;
+  
   test('Add Task', () => {
-    addTask({
-      taskDescrip: addInput.value, completed: false, index: TASK.TASKarr.length + 1,
-    });
+    document.body.innerHTML = htmlNoTask;
+    const addInput = document.getElementById('addinput');
+    addInput.setAttribute('value', 'Some input');
+
+    addTask();
+
     expect(getStorage()).toEqual([{
-      taskDescrip: 'Some imput',
+      taskDescrip: 'Some input',
       completed: false,
-      index: TASK.TASKarr.length + 1,
+      index: 1,
     }]);
   });
 });
