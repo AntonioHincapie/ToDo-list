49635d8c7cdb275a0d84aee2d8e692bc74620590
lucascr <lcrodrigues1995@gmail.com>
2022-05-19 17:14:35 -0300

    Add "clear all completed" test

--- src/modules/clear-modify-check.test.js
+++ src/modules/clear-modify-check.test.js
@@ -1,6 +1,7 @@
 const { setTasks } = require('./functions.js');
 const { getTasks } = require('./functions.js');
 const { updateTask } = require('./functions.js');
+const { clearAllBtn } = require('./functions.js');
 
 describe('Modify, Clear All, If Checked in Storage', () => {
   test('Modify the value of the task', () => {
@@ -16,5 +17,33 @@ describe('Modify, Clear All, If Checked in Storage', () => {
     expect(items).toBeDefined();
     expect(items).toHaveLength(1);
     expect(items[0].description).toBe(newValue);
+  });
+
+  test('Clear all completed tasks', () => {
+    setTasks([{
+      description: 'Task 1',
+      isCompleted: true,
+      index: 1,
+      id: 1,
+    },
+    {
+      description: 'Task 2',
+      isCompleted: false,
+      index: 2,
+      id: 2,
+    },
+    {
+      description: 'Task 3',
+      isCompleted: true,
+      index: 3,
+      id: 3,
+    },
+    ]);
+    clearAllBtn();
+    const items = getTasks();
+    expect(items).toBeDefined();
+    expect(items).toHaveLength(1);
+    expect(items[0].isCompleted).toBe(false);
+    expect(items[0].index).toEqual(1);
   });
 });
